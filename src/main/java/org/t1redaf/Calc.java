3e1579ee56143e1a1f4289d6f97783e59ec1f0a0
MHe-axyeHHo <alba20001000@gmail.com>
2022-12-17 17:02:30 +0500

    добавил в класс Calc гетеро-сетеры

--- src/main/java/org/t1redaf/Calc.java
+++ src/main/java/org/t1redaf/Calc.java
@@ -1,13 +1,38 @@
 package org.t1redaf;
 
 public class Calc {
-    public static void ras(double RV, double r, int n, int pop){
-        double sum = 0;//начислено процентов
+    private double RV;
+    private double r;
+    private int n;
+    private int pop;
+    int popCount = 0;
+    double sum = 0;
+
+    public void setAll(double a, double b, int c, int d){
+        RV = a;
+        r = b;
+        n = c;
+        pop = d;
+    }
+    public String getRV(){
+        String RVString = Double.toString(RV);
+        return RVString;
+    }
+    public String getProc(){
+        String proc = Double.toString(sum-popCount);
+        return proc;
+    }
+    public String getPop(){
+        String popString = Double.toString(popCount);
+        return popString;
+    }
+    public void ras(){
+
         double month = 1 + (r / 12);//процентная ставка
         double bac;//переменная для сохранения предыдущего значения вклада для вычитания
         pop = -500;//пополнение или снятие
-        int popCount = 0;//пополнение или снятие за весь период
-        RV = 100000;
+
+        //RV = 100000;
         // Math.pow((1+r/12),n);
         //в зависимости от снятия или пополнения значение pop будет отрицательным или положительным
         for (int i = 0; i<n; i++){
@@ -16,6 +41,7 @@ public class Calc {
             RV = (RV * month)+pop;
             sum = sum + RV - bac;
         }
-        System.out.println("Остаток вклада: "+ RV+" Начислено процентов: "+(sum-popCount)+" Пополнено (или снято): "+(popCount));
+        //System.out.println("Остаток вклада: "+ RV+" Начислено процентов: "+(sum-popCount)+" Пополнено (или снято): "+(popCount));
     }
+
 }
