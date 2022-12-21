2a3e49c3b5d17cc854a01b9ff38e47d4b8b074cc
njmin <gazinurnurtdinov35@gmail.com>
2022-12-21 15:50:38 +0300

    Сделал ПДФ по красоте

--- src/main/java/org/t1redaf/PdfCreator.java
+++ src/main/java/org/t1redaf/PdfCreator.java
@@ -3,8 +3,6 @@ package org.t1redaf;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
-import javafx.scene.text.TextAlignment;
-
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -12,12 +10,11 @@ import java.util.List;
 
 
 public class PdfCreator {
-
+    //TODO бан за полный путь
     public static final Font FONT = FontFactory.getFont("C:\\\\WINDOWS\\Fonts\\\\ARIAL.TTF","CP1251",true);//шрифт
 
     public static void create(List<Double> procents, List<Double> ostatok,int popolnenie, String replenishmentOrPayment,DepositDTO depositDTO) {//статический метод
         String[] tableHeads = {"Месяц","Проценты (руб)",replenishmentOrPayment,"Остаток (руб)"};//шапка пдф документа
-        String tableFooter = "Создано командой разработчиков №3";
         Document doc = new Document();//иницализируем документ
 
         Image image = null;
@@ -34,30 +31,35 @@ public class PdfCreator {
             PdfWriter writer = PdfWriter.getInstance(doc, new FileOutputStream("Результат расчета.pdf"));
             doc.open();
 
-            doc.add(new Paragraph(depositDTO.toString(), FONT));
+            Paragraph firstLine = new Paragraph("ОТЧЕТ О РАБОТЕ ПРОГРАММЫ: \n\n", FONT);
+            firstLine.setAlignment(Element.ALIGN_CENTER);
+            doc.add(firstLine);
 
+            doc.add(new Paragraph(depositDTO.toString(), FONT));
             PdfPTable table = new PdfPTable(4);
 
-            image.setAbsolutePosition(400, 730);
+            image.setAbsolutePosition(475, 725);
             writer.getDirectContent().addImage(image);
+            doc.add(image);
 
             for (String tableHead : tableHeads) {
                 table.addCell(new Phrase(tableHead,FONT));
             }
             for (int i = 0; i < procents.size(); i++) {
                 table.addCell(Integer.toString(i+1));
-                table.addCell(procents.get(i).toString());
+                table.addCell(String.format("%.2f", procents.get(i)));
                 table.addCell(Integer.toString(depositDTO.getPopolnenie()));
-                table.addCell(ostatok.get(i).toString());
+                table.addCell(String.format("%.2f",ostatok.get(i)));
             }
-
-
-
-            doc.add(image);
             doc.add(table);
-            doc.add((new Phrase(tableFooter,FONT)));
+
+            Paragraph footer = new Paragraph("Создано командой разработчиков №3", FONT);
+            footer.setAlignment(Element.ALIGN_CENTER);
+            doc.add(footer);
+
             doc.close();
             writer.close();
+
         } catch (DocumentException | FileNotFoundException e) {
             throw new RuntimeException(e);
         }
